8215cf32f6a4923a5ab79ed306ce121834cff6a1
Quanluis <quanluis@protonmail.com>
2019-04-03 14:43:50 -0400

    deleted unwanted files

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# toDoList
+Js To-do list 
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <h1>To-Do List App</h1>
+      <input type="text"/>
+      <button>Add</button>
+    </div>
+    );
+  }
+}
+
+export default App;
