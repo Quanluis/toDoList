0c485631682333295e52c27c331105fa5c9d743e
Quanluis <quanluis@protonmail.com>
2019-04-04 13:19:27 -0400

    Modified files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  
+
   render() {
+
+    let input = document.getElementById('input')
+
+    function newInput(){
+      
+    }
+
+    
+
     return (
     <div>
       <h1>To-Do List App</h1>
-      <input type="text"/>
-      <button>Add</button>
+      <input id ="input" type="tex"/>
+      <button   className = 'inputone'  >Add</button>
     </div>
     );
   }
